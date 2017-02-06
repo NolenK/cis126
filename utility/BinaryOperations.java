cf34d2e7e8a154eda676ab0915b8648ec97d4e6b
David J Castner <davidjcastner@gmail.com>
2017-02-06 14:19:58 -0500

    finish outline for binary conversions

---
+++ utility/BinaryOperations.java
@@ -0,0 +1,33 @@
+package utility;
+
+public class BinaryOperations {
+    // boolean arrays store the binary data with the index representing the bit position
+    // the least significant bit starts at position 0
+    // and increases as the significance increases
+    // binaryData[0] is the least significant bit
+    // binaryData[binaryData.length - 1] is the most significant bit
+
+    public static boolean[] convertToArray(String binaryData) {
+        return null;
+    }
+
+    public static boolean[] convertToArray(int binaryData, int numberOfBits) {
+        return null;
+    }
+
+    public static int convertToInteger(String binaryData) {
+        return 0;
+    }
+
+    public static int convertToInteger(boolean[] binaryData) {
+        return 0;
+    }
+
+    public static String convertToString(int binaryData, int numberOfBits) {
+        return "";
+    }
+
+    public static String convertToString(boolean[] binaryData) {
+        return "";
+    }
+}
