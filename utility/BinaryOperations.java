373f2e80c11fdd01dd444ef9577b1febadbc2865
rachelramin <rachelramin1@gmail.com>
2017-02-08 15:15:46 -0500

    Added my methods
    First 3:
    convertToArray(String binaryData)
    convertToArray(int binaryData, int numberOfBits)
    convertToInteger(String binaryData)

--- utility/BinaryOperations.java
+++ utility/BinaryOperations.java
@@ -7,35 +7,75 @@ public class BinaryOperations {
     // binaryData[0] is the least significant bit
     // binaryData[binaryData.length - 1] is the most significant bit
 
-    public static boolean[] convertToArray(String binaryData) {
+    public static boolean[] convertToArray(String binaryData) 
+    {
+         //create an output array based off length of imported string.
         boolean[] output = new boolean[binaryData.length()];
-        for (int i = 0; i < binaryData.length(); i++) {
-            int charIndex = binaryData.length() - 1 - i;
-            output[i] = binaryData.substring(charIndex, charIndex + 1).equals("1");
-        }
-        return output;
-    }
-
-    public static boolean[] convertToArray(int binaryData, int numberOfBits) {
-        boolean[] output = new boolean[numberOfBits];
-        for (int i = 0; i < numberOfBits; i++) {
-            output[i]  = (binaryData & 1) == 1;
-            binaryData = binaryData >> 1;
-        }
-        return output;
-    }
-
-    public static int convertToInteger(String binaryData) {
-        int output = 0;
-
-        while (binaryData.length() > 0) {
-            output = output << 1;
-            if (binaryData.substring(0, 1).equals("1")) {
-                output = output | 1;
+        
+        //create a string array from string, using split to split 
+        String [] parts = binaryData.split("");
+        
+        //for loop that irrterates for as many times as long as the string array parts is.
+        for (int i = 0; i < parts.length; i++)
+        {
+            //if the char at the index point i = 1 the output at that index point is true.
+            if(parts[i].equals("1"))
+            {
+               output[i] = true;
             }
-            binaryData = binaryData.substring(1);
+            
+            //else the output is false at that index.
+            else
+            {
+               output[i] = false;
+            }
+            
         }
+         
         return output;
+
+    }
+
+    public static boolean[] convertToArray(int binaryData, int numberOfBits) 
+    {
+        boolean[] output = new boolean[numberOfBits]; 
+       
+       //convert int to string.
+       String binaryString = Integer.toString(binaryData);
+       
+       //formats the String to add 0s in binary, based on the number of user required bits.       
+       binaryString = String.format("%"+Integer.toString(numberOfBits)+"s",Integer.toBinaryString(19)).replace(" ","0");
+       
+       //create a string to help the sorting process.        
+       String [] parts = binaryString.split("");
+              
+       //for loop that irrterates for as many times as long as the string array parts is.
+        for (int i = 0; i < parts.length; i++)
+        {
+            //if the char at the index point i = 1 the output at that index point is true.
+            if(parts[i].equals("1"))
+            {
+               output[i] = true;
+            }
+            
+            //else the output is false at that index.
+            else
+            {
+               output[i] = false;
+            }
+
+        }
+
+        return output;
+    }
+
+    public static int convertToInteger(String binaryData) 
+    {
+        //creates the int varible output, and parses the string value into the int. 
+        int output = Integer.parseInt(binaryData);
+         
+        return output;
+
     }
 
     public static int convertToInteger(boolean[] binaryData) {
